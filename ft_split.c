6202f7085e39ce4e229db5f9af11e9a37a1f6e4c
Arnaud Huge <ahuge@1B4.42angouleme.fr>
2023-11-06 16:01:13 +0100

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,13 @@
 /*   By: ahuge <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 15:12:08 by ahuge             #+#    #+#             */
-/*   Updated: 2023/11/02 13:25:16 by ahuge            ###   ########.fr       */
+/*   Updated: 2023/11/06 16:00:06 by ahuge            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
 
 int	count_word(char const *s, char c)
 {
@@ -22,46 +23,76 @@ int	count_word(char const *s, char c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] != c && s[i + 1] == c || s[i + 1] == '\0')
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
 			x++;
-
 		i++;
 	}
 	return (x);
 }
 
+void	ft_tabcpy(char *dest, char const *src, int c)
+{
+	int	i;
 
-char **ft_split(char const *s, char c)
+	i = 0;
+	while (src[i] != c)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+void	ft_tabset(char **tab, const char *s, int c)
+{
+	int	i;
+	int	x;
+	int	j;
+
+	i = 0;
+	x = 0;
+	j = 0;
+	while (s[i])
+	{
+		x = 0;
+		while (s[i + x] && s[i + x] != c)
+			x++;
+		if (x > 0)
+		{
+			tab[j] = malloc(sizeof(char) * (x + 1));
+			ft_tabcpy(tab[j], &s[i], c);
+			j++;
+			i = i + x;
+		}
+		else
+			i++;
+	}
+	tab[j] = NULL;
+}
+
+char	**ft_split(char const *s, char c)
 {
 	int		x;
 	int		i;
 	int		j;
+	int		y;
 	char	**tab;
 
 	i = 0;
 	j = 0;
+	y = 0;
 	x = count_word(s, c);
-	printf("%d\n", x);
-	tab = malloc(sizeof(char*) * (x + 1));
-	while (i <= x)
-	{
-		if (s[i] != c && s[i + 1] == c || s[i + 1] == '\0')
-		{
-			tab[j] = 
-			x++;
-		}
-			
-	}
-	
+	tab = malloc(sizeof(char *) * (x + 1));
+	ft_tabset(tab, s, c);
 	return (0);
 }
 
-
+/*
 int main()
 {
-	char *tab = "Ceci,est,un,test";
+	char *tab = "Ceci, est, un, test";
 	char c = ',';
 
 	ft_split(tab, c);
 }
-
+*/
